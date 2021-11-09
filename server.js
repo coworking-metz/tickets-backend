e470472d9f3358ba50ce31235a6a60f96fdf07bb
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-11-09 15:21:09 +0100

    Add /api/notify route

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongo = require('./lib/mongo')
 const w = require('./lib/w')
 const cache = require('./lib/cache')
 const netatmo = require('./lib/netatmo')
-const {coworkersNow, getUserStats, getUserPresences, heartbeat, getMacAddresses, getCollectionsData, updatePresence} = require('./lib/api')
+const {coworkersNow, getUserStats, getUserPresences, heartbeat, getMacAddresses, getCollectionsData, updatePresence, notify} = require('./lib/api')
 const {checkKey} = require('./lib/auth')
 
 const {computeStats, computePeriodsStats, asCsv} = require('./lib/stats')
@@ -76,6 +76,7 @@ app.post('/api/heartbeat', checkKey(process.env.PRESENCE_API_KEY), express.urlen
 app.post('/api/mac', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(getMacAddresses))
 app.post('/api/presence', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(updatePresence))
 app.post('/api/collections-data', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(getCollectionsData))
+app.post('/api/notify', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(notify))
 
 const port = process.env.PORT || 5000
 
