416b28ad71b384705288a35baf14ea33afea7df9
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-11-09 11:14:56 +0100

    Add /api/collections-data route

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongo = require('./lib/mongo')
 const w = require('./lib/w')
 const cache = require('./lib/cache')
 const netatmo = require('./lib/netatmo')
-const {coworkersNow, getUserStats, getUserPresences, heartbeat, getMacAddresses} = require('./lib/api')
+const {coworkersNow, getUserStats, getUserPresences, heartbeat, getMacAddresses, getCollectionsData} = require('./lib/api')
 const {checkKey} = require('./lib/auth')
 
 const {computeStats, computePeriodsStats, asCsv} = require('./lib/stats')
@@ -74,6 +74,7 @@ app.post('/api/user-presences', checkKey(process.env.PURCHASE_API_KEY), express.
 
 app.post('/api/heartbeat', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(heartbeat))
 app.post('/api/mac', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(getMacAddresses))
+app.post('/api/collections-data', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(getCollectionsData))
 
 const port = process.env.PORT || 5000
 
