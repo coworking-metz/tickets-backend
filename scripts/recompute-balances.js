240771303bbbfda9c8dd8bb12eadb4541d5e87a7
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-11-13 14:38:53 +0100

    Add script to recompute all balances

---
+++ scripts/recompute-balances.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+require('dotenv').config()
+
+const mongo = require('../lib/mongo')
+const {updateBalance} = require('../lib/models')
+
+async function main() {
+  await mongo.connect()
+  const userIds = await mongo.db.collection('users').distinct('_id')
+
+  for (const userId of userIds) {
+    /* eslint-disable-next-line no-await-in-loop */
+    const balance = await updateBalance(userId)
+    console.log(`${userId} : ${balance}`)
+  }
+
+  console.log('Completed!')
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
