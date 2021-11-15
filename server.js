8b80d4c80414814c3497fd5f4e6d828fa3c28e3f
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-11-15 15:52:54 +0100

    Add /api/users-stats route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const mongo = require('./lib/util/mongo')
 const w = require('./lib/util/w')
 const cache = require('./lib/cache')
 const netatmo = require('./lib/netatmo')
-const {coworkersNow, getUserStats, getUserPresences, heartbeat, getMacAddresses, getCollectionsData, updatePresence, notify, purchaseWebhook} = require('./lib/api')
+const {coworkersNow, getUserStats, getUserPresences, heartbeat, getMacAddresses, getCollectionsData, updatePresence, notify, purchaseWebhook, getUsersStats} = require('./lib/api')
 const {checkKey} = require('./lib/auth')
 
 const {computeStats, computePeriodsStats, asCsv} = require('./lib/stats')
@@ -96,6 +96,9 @@ async function main() {
 
   app.get('/api/user-presences', checkKey(process.env.PURCHASE_API_KEY), w(getUserPresences))
   app.post('/api/user-presences', checkKey(process.env.PURCHASE_API_KEY), express.urlencoded({extended: false}), w(getUserPresences))
+
+  app.get('/api/users-stats', checkKey(process.env.PURCHASE_API_KEY), w(getUsersStats))
+  app.post('/api/users-stats', express.urlencoded({extended: false}), checkKey(process.env.PURCHASE_API_KEY), w(getUsersStats))
 
   app.post('/api/heartbeat', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(heartbeat))
   app.post('/api/mac', checkKey(process.env.PRESENCE_API_KEY), express.urlencoded({extended: false}), w(getMacAddresses))
