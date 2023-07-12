a651542a11ad0088b8228e51bf3b6236abdf0e30
Gilles FRANCOIS <gilles@sopress.net>
2023-07-12 09:42:39 +0200

    doc routes api + log interne

--- server.js
+++ server.js
@@ -11,16 +11,17 @@ const { add } = require('date-fns')
 const MongoStore = require('connect-mongo')
 const passport = require('passport')
 const got = require('got')
-
+const path = require('path');
 const mongo = require('./lib/util/mongo')
 const w = require('./lib/util/w')
+const { ip } = require('./lib/util/network')
 const errorHandler = require('./lib/util/error-handler')
 const cache = require('./lib/cache')
 const netatmo = require('./lib/netatmo')
 const { coworkersNow, resolveUser, getUserStats, getUserPresences, heartbeat, getMacAddresses, getMacAddressesLegacy, getCollectionsData, updatePresence, notify, purchaseWebhook, getUsersStats, getCurrentUsers, getVotingCoworkers } = require('./lib/api')
 const { checkToken } = require('./lib/auth')
 const { connexion, checkSession, deleteSession } = require('./lib/connexion')
-
+const { ouvrirPortail } = require('./lib/portail');
 const { parseFromTo } = require('./lib/dates')
 const { computeIncomes } = require('./lib/models')
 const { computeStats, computePeriodsStats, asCsv } = require('./lib/stats')
@@ -33,6 +34,8 @@ async function main() {
   require('./lib/util/passport').config()
 
   const app = express()
+
+  app.use('/doc', express.static(path.join(__dirname, 'doc')));
 
   app.use(cors({ origin: true }))
 
@@ -64,11 +67,16 @@ async function main() {
     res.send(stats)
   }))
 
-
   /**
-   * NETATMO  
+   * @apiDefine netatmo Netatmo
+   * These endpoints relate to the Netatmo weather stations
    */
 
+  /**
+   * @api {get} /netatmo/stations Request weather station data
+   * @apiGroup netatmo
+   * @apiSuccess {Object[]} stations The list of weather stations
+   */
   app.get('/netatmo/stations', w(async (req, res) => {
     if (!netatmo.isAvailable()) {
       return res.status(500).send({ code: 500, message: 'Non disponible. Netatmo n’est pas configuré.' })
@@ -80,9 +88,24 @@ async function main() {
 
 
   /**
-   * STATS  
+   * @apiDefine stats Stats
+   * Endpoints for accessing statistics
    */
   const PERIODS_TYPES = new Set(['day', 'week', 'month', 'year'])
+
+
+  /**
+   * @api {get} /stats Request global stats
+   * @apiGroup stats
+   * @apiSuccess {Object} stats The global stats
+   */
+
+  /**
+   * @api {get} /stats/:periodType Request stats for a specific period
+   * @apiGroup stats
+   * @apiParam {String} periodType The type of period (day, week, month, year)
+   * @apiSuccess {Object} stats The stats for the specified period
+   */
   app.get('/stats/:periodType', w(async (req, res) => {
     const { periodType } = req.params
 
@@ -107,6 +130,12 @@ async function main() {
     res.send(stats)
   }))
 
+  /**
+ * @api {get} /stats/incomes/:periodType Request income stats for a specific period
+ * @apiGroup stats
+ * @apiParam {String} periodType The type of period (day, week, month, year)
+ * @apiSuccess {Object} stats The income stats for the specified period
+ */
   app.get('/stats/incomes/:periodType', w(async (req, res) => {
     const { periodType } = req.params
 
@@ -135,36 +164,137 @@ async function main() {
 
 
   /**
-   * COWORKERS & PRESENCES  
+   * @apiDefine coworkers Coworkers & Presences
+   * Endpoints for managing coworkers and their presences
    */
 
+  /**
+   * @api {get} /coworkers-now Request current coworkers
+   * @apiGroup coworkers
+   * @apiSuccess {Object[]} coworkers The list of current coworkers
+   */
   app.get('/coworkersNow', w(coworkersNow))
   app.post('/coworkersNow', w(coworkersNow))
-
   app.get('/api/coworkers-now', w(coworkersNow))
   app.post('/api/coworkers-now', w(coworkersNow))
 
+
+  /**
+   * @api {get} /api/user-stats Get User Statistics
+   * @apiGroup User
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {Object} User statistics.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.get('/api/user-stats', checkToken(adminTokens), w(resolveUser), w(getUserStats))
   app.post('/api/user-stats', express.urlencoded({ extended: false }), checkToken(adminTokens), w(resolveUser), w(getUserStats))
+
+  /**
+   * @api {get} /api/users/:userId/stats Get Specific User's Statistics
+   * @apiGroup User
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiParam {String} userId User's unique ID.
+   * @apiSuccess {Object} User statistics.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.get('/api/users/:userId/stats', checkToken(adminTokens), w(resolveUser), w(getUserStats))
 
+
+  /**
+   * @api {get} /api/user-presences Get User Presences
+   * @apiGroup User
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {Object} User presences.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.get('/api/user-presences', checkToken(adminTokens), w(resolveUser), w(getUserPresences))
   app.post('/api/user-presences', express.urlencoded({ extended: false }), checkToken(adminTokens), w(resolveUser), w(getUserPresences))
+
+  /**
+   * @api {get} /api/users/:userId/presences Get Specific User's Presences
+   * @apiGroup User
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiParam {String} userId User's unique ID.
+   * @apiSuccess {Object} User presences.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.get('/api/users/:userId/presences', checkToken(adminTokens), w(resolveUser), w(getUserPresences))
 
+  /**
+   * @api {get} /api/voting-coworkers Get Voting Coworkers
+   * @apiGroup User
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {Object} Voting coworkers data.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.get('/api/voting-coworkers', checkToken(adminTokens), w(getVotingCoworkers))
 
+  /**
+   * @api {post} /api/users-stats Get All Users' Stats
+   * @apiGroup User
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {Object} Users' statistics data.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.get('/api/users-stats', checkToken(adminTokens), w(getUsersStats))
   app.post('/api/users-stats', express.urlencoded({ extended: false }), checkToken(adminTokens), w(getUsersStats))
 
+  /**
+   * @api {get} /api/current-users Get Current Users
+   * @apiGroup User
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {Object} Current users data.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.get('/api/current-users', checkToken(adminTokens), w(getCurrentUsers))
   app.post('/api/current-users', express.urlencoded({ extended: false }), checkToken(adminTokens), w(getCurrentUsers))
-
+  /**
+   * @api {post} /api/heartbeat Post Heartbeat
+   * @apiGroup System
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {String} Success message.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.post('/api/heartbeat', express.urlencoded({ extended: false }), checkToken(adminTokens), w(heartbeat))
+
+  /**
+   * @api {post} /api/presence Update Presence
+   * @apiGroup User
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {String} Success message.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.post('/api/presence', express.urlencoded({ extended: false }), checkToken(adminTokens), w(updatePresence))
+
+  /**
+   * @api {post} /api/collections-data Get Collections Data
+   * @apiGroup Data
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {Object} Collections data.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.post('/api/collections-data', express.urlencoded({ extended: false }), checkToken(adminTokens), w(getCollectionsData))
+
+  /**
+   * @api {post} /api/notify Post Notify
+   * @apiGroup Notifications
+   * @apiHeader {String} Authorization Admin's token.
+   * @apiSuccess {String} Success message.
+   * @apiError {String} 401 Unauthorized Invalid token.
+   */
   app.post('/api/notify', express.urlencoded({ extended: false }), checkToken(adminTokens), w(notify))
 
+  /**
+ * @api {post} /api/purchase-webhook Post Purchase Webhook
+ * @apiGroup Webhook
+ * @apiDescription This API endpoint validates and parses JSON data. It computes a signature using 'sha256' algorithm and compares it with the 'x-wc-webhook-signature' from the request header. If they do not match, it throws an error.
+ *
+ * @apiHeader {String} x-wc-webhook-signature Signature of the webhook, needed for verification.
+ *
+ * @apiError (Error 400) {String} WebhookSignatureMismatch Error message when the computed signature does not match the provided webhook signature.
+ *
+ * @apiSuccess {String} success Success message upon successful validation and parsing.
+ */
   const validateAndParseJson = express.json({
     verify(req, res, buf) {
       const computedSignature = crypto.createHmac('sha256', process.env.WP_WC_WEBHOOK_SECRET)
@@ -177,16 +307,17 @@ async function main() {
     }
   })
   app.post('/api/purchase-webhook', validateAndParseJson, w(purchaseWebhook))
-
   /**
-   * MAC ADDRESS  
+   * @apiDefine mac Adresses MAC
+   * Endpoints for managing coworkers mac addresses
    */
   app.get('/api/mac', checkToken(adminTokens), w(getMacAddresses))
   app.post('/api/mac', express.urlencoded({ extended: false }), checkToken(adminTokens), w(getMacAddressesLegacy))
 
 
   /**
-   * COWO API 
+   * @apiDefine cowo Cowo APi
+   * Endpoints for managing all coworking related data (login, sessions, etc.)
    */
   app.post('/api/connexion', express.urlencoded({ extended: false }), checkToken(adminTokens), connexion)
   app.post('/api/session', express.urlencoded({ extended: false }), checkToken(adminTokens), checkSession)
@@ -215,11 +346,19 @@ async function main() {
   })
 
   /**
-   * OUVERTURE PORTAIL PAR INTERPHONE 
+   * @apiDefine portail Portail
+   * Endpoints for managing the front door
    */
-
+  /**
+   * @api {post} /api/interphone Request to Open the Gate
+   * @apiName OpenGate
+   * @apiGroup Interphone
+   * 
+   * @apiHeader {String} Authorization Admin's token.
+   * 
+   */
   app.post('/api/interphone', checkToken(adminTokens), w(async (req, res) => {
-    await got.post(process.env.INTERPHONE_URL)
+    ouvrirPortail(req);
     res.status(202).send({ message: 'Ouverture du portail demandée' })
   }))
 
