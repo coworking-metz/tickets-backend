aae347bde398219149efacad253466bd6730b863
Gilles FRANCOIS <gilles@sopress.net>
2024-04-24 12:25:18 +0200

    Utilisations des nouvelles collections dans les fonctions getMemberXXX (et ajout d'une nouvelle route pour recuperer les memberships d'un membre)

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ import {
   getMemberPresences,
   getMemberTickets,
   getMemberSubscriptions,
+  getMemberMemberships,
   heartbeat,
   getMacAddressesLegacy,
   updatePresence,
@@ -113,6 +114,7 @@ app.get('/api/members/:userId', w(multiAuth), w(getMemberInfos))
 app.get('/api/members/:userId/activity', w(multiAuth), w(getMemberActivity))
 app.get('/api/members/:userId/tickets', w(multiAuth), w(getMemberTickets))
 app.get('/api/members/:userId/subscriptions', w(multiAuth), w(getMemberSubscriptions))
+app.get('/api/members/:userId/memberships', w(multiAuth), w(getMemberMemberships))
 app.put('/api/members/:userId/mac-addresses', express.json(), w(multiAuth), w(updateMemberMacAddresses))
 app.post('/api/members/:userId/sync-wordpress', w(multiAuth), w(forceWordpressSync))
 
